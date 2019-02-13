2b98e82df801ece02011e3782360ac62272f7a7f
David Paul <david.crouch@gladney.org>
2019-02-13 16:12:07 -0600

    updated customizer class

--- lib/classes/customizer.class.php
+++ lib/classes/customizer.class.php
@@ -7,16 +7,21 @@ defined( 'ABSPATH' ) || die;
 class Customizer {
 
 	public static function init() {
-		add_action( 'customize_register', [ __CLASS__, 'register_customizer' ] );
+		add_action( 'customize_register', [ __CLASS__, 'register_customizer' ], 99 );
 	}
 
 	public static function register_customizer( $wp_customize ) {
+		// cleanup
+		$wp_customize->remove_section('colors');
+		$wp_customize->remove_section('custom_css');
+		$wp_customize->remove_section('static_front_page');
+
 		$wp_customize->add_panel( 'gcfa_footer', [
 			'title' => 'Footer',
 			'capability'     => 'edit_theme_options',
 			'theme_supports' => '',
 			'description' => 'Control the content of the site footer',
-			'priority' => 60
+			'priority' => 130
 		]);
 	}
 }
