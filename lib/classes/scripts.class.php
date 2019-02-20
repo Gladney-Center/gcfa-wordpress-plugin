181f24348a9b749847ea233843b7371e87e66514
David Paul <david.crouch@gladney.org>
2019-02-20 10:28:48 -0600

    Added auto enqueue of theme script

--- lib/classes/scripts.class.php
+++ lib/classes/scripts.class.php
@@ -8,8 +8,13 @@ class Scripts {
     public static function init() {
         wp_dequeue_script('jquery');
         wp_deregister_script('jquery');
+
+        $theme_name = end(explode('/',get_stylesheet_directory()));
+
+        //styles
+        wp_enqueue_script('gladney-main', get_stylesheet_directory_uri()."/$theme_name.min.js");
         
         //styles
-        wp_enqueue_style('gladney-main', get_stylesheet_directory_uri().'/style.css', false, null);
+        wp_enqueue_style('gladney-main', get_stylesheet_directory_uri().'/style.min.css', false, null);
     }
 }
