04a37aa822e71dd4f25a574c718e150467c69a5f
David Paul <david.crouch@gladney.org>
2019-03-15 13:12:31 -0500

    Added dequeue and deregister of block styles on the front-end

--- lib/classes/scripts.class.php
+++ lib/classes/scripts.class.php
@@ -6,8 +6,15 @@ defined( 'ABSPATH' ) || die;
 
 class Scripts {
     public static function init() {
+        // git rid of default jQuery
         wp_dequeue_script('jquery');
         wp_deregister_script('jquery');
+
+        // get rid of block styles on the front end (we'll control our own thankyouverymuch)
+        wp_dequeue_style('wp-block-library');
+        wp_dequeue_style('editor-blocks');
+        wp_deregister_style('wp-block-library');
+        wp_deregister_style('editor-blocks');
 
         $theme_name = @end(explode('/',get_stylesheet_directory()));
 
